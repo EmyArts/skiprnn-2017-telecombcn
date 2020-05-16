6ae367014384dff6c0dc0292ea73df9ef7cad2e6
EmyArts <emy.msn.arts@hotmail.com>
2020-05-16 19:41:42 +0200

    Changing gamma to 0.01

--- src/util/graph_definition.py
+++ src/util/graph_definition.py
@@ -27,7 +27,7 @@ def create_generic_flags():
     tf.app.flags.DEFINE_string('logdir', '../logs', "Directory where TensorBoard logs will be stored.")
 
     # Flags for the Skip RNN cells
-    tf.app.flags.DEFINE_float('cost_per_sample', 0.01, "Cost per used sample. Set to 0 to disable this option.")
+    tf.app.flags.DEFINE_float('cost_per_sample', 0.0001, "Cost per used sample. Set to 0 to disable this option.")
 
 
 def compute_gradients(loss, learning_rate, gradient_clipping=-1):
