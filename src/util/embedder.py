6be69b4c7640f5f1a096dd16e05313e5bfb7f917
EmyArts <emy.msn.arts@hotmail.com>
2020-05-23 12:13:32 +0200

    Variable_scope compat

--- src/util/embedder.py
+++ src/util/embedder.py
@@ -77,7 +77,7 @@ class Embedding:
 			inputs.append(inp)
 			ps.append(p)
 			l.append(label)
-		return tf.data.Dataset.from_tensors((np.array(inputs), np.array(ps), np.array(l)))
+		return tf.data.Dataset.from_tensor_slices((np.array(inputs), np.array(ps), np.array(l)))
 
 
 
