06bbc54c91d949ce981242ee38569429cc597fcb
Emy <8arts@informatik.uni-hamburg.de>
2020-07-08 13:33:57 +0200

    Complete new code, let's hope

--- src/04_imdb_2.py
+++ src/04_imdb_2.py
@@ -125,7 +125,7 @@ def train():
     # Split the outputs of the RNN into the actual outputs and the state update gate
     rnn_outputs, updated_states = split_rnn_outputs(FLAGS.model, rnn_outputs)
 
-    print(f"\nUpdated states are {updated_states}.\n")
+    # print(f"\nUpdated states are {updated_states}.\n")
 
     logits = layers.linear(inputs=rnn_outputs[:, -1, :], num_outputs=OUTPUT_SIZE)
     predictions = tf.argmax(logits, 1)
@@ -176,12 +176,12 @@ def train():
             for iteration in range(ITERATIONS_PER_EPOCH):
                 # Perform SGD update
                 # print(train_matrix[iteration].shape, train_matrix[iteration].dtype)
-                sess.run([train_fn, loss], feed_dict={samples: train_matrix[iteration], ground_truth: train_labels[iteration]})
+                sess.run([train_fn], feed_dict={samples: train_matrix[iteration], ground_truth: train_labels[iteration]})
                 # _, loss =
                 # loss = sess.run(loss)
                 # # sess.run(train_model_spec['samples'])
                 # print(loss)
-                train_acc_plt[epoch][iteration] = loss
+                # train_acc_plt[epoch][iteration] = loss
                 # test_iter_accuracy, test_iter_loss, test_used_inputs= sess.run([accuracy, loss, updated_states], feed_dict={samples: test_inputs[iteration], ground_truth: test_inputs[iteration]})
             duration = time.time() - start_time
 
