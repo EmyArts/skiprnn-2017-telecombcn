0e4fc356c7ba4fe52f23040955fbfeab3bf91b49
Emy <8arts@informatik.uni-hamburg.de>
2020-07-08 13:37:29 +0200

    Complete new code, let's hope

--- src/04_imdb_2.py
+++ src/04_imdb_2.py
@@ -175,7 +175,7 @@ def train():
             start_time = time.time()
             for iteration in range(ITERATIONS_PER_EPOCH):
                 # Perform SGD update
-                # print(train_matrix[iteration].shape, train_matrix[iteration].dtype)
+                print(train_labels[iteration])
                 sess.run([train_fn], feed_dict={samples: train_matrix[iteration], ground_truth: train_labels[iteration]})
                 # _, loss =
                 # loss = sess.run(loss)
