b8ec93b8e115970dfe6b08035c1cec53f2c1eeaf
Emy <emy.arts@informatik.uni-hamburg>
2020-07-10 09:16:45 +0200

    A new hope... Maybe finally

--- src/util/graph_definition.py
+++ src/util/graph_definition.py
@@ -135,7 +135,7 @@ def compute_surprisal_loss(model, loss, updated_states, sample_probabilities, su
     print(updated_states)
     if using_skip_rnn(model):
         neg_updated_states = tf.subtract(tf.ones(updated_states.shape, dtype=tf.dtypes.float32), updated_states)
-        surprisal = tf.multiply(neg_updated_states, -(tf.log(sample_probabilities)))
+        surprisal = tf.multiply(neg_updated_states, - (tf.log(sample_probabilities)))
         average_surprisal = tf.divide(surprisal, tf.reduce_sum(neg_updated_states))
         return tf.reduce_mean(surprisal_influence * average_surprisal)
     else:
