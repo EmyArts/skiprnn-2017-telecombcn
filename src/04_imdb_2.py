807c948846c2243941a84afa14f286ba614ba52a
Emy <emy.arts@informatik.uni-hamburg>
2020-07-10 22:58:38 +0200

    A new hope... Maybe finally

--- src/04_imdb_2.py
+++ src/04_imdb_2.py
@@ -257,7 +257,8 @@ def train():
                                                    100. * test_steps / SEQUENCE_LENGTH))
 
             loss_perc = loss_plt[epoch, :, :].mean()
-            loss_perc = (loss_perc / loss_perc.sum()) * 100
+            loss_perc = np.divide(loss_perc, (loss_perc.sum())) * 100
+            print(loss_perc)
             print(f"entropy: {loss_perc[0]}%, budget: {loss_perc[1]}%, surprisal: {loss_perc[2]}%.")
 
             # print(f"entropy: {loss_plt[epoch, :, 0].mean()}, budget: {loss_plt[epoch, :, 1].mean()}, surprisal: {loss_plt[epoch, :, 2].mean()}.")
