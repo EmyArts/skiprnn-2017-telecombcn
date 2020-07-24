73641d507c2a1cca102842f3f445860cd36080f7
EmyArts <emy.msn.arts@hotmail.com>
2020-07-24 13:39:21 +0200

    Trying to fix the NaN problem

--- src/util/graph_definition.py
+++ src/util/graph_definition.py
@@ -124,11 +124,11 @@ def compute_budget_loss(model, loss, updated_states, cost_per_sample):
         #     return sample_loss
         # else:
         #     return tf.reduce_sum(cost_per_sample * updated_states, 1)
-        print = tf.cond(tf.math.is_nan(sample_loss),
-                        true_fn=lambda: tf.Print(sample_loss, [sample_loss], "Sample loss is null "),
-                        false_fn=lambda: tf.no_op())
-        with tf.control_dependencies([print]):
-            return sample_loss
+        # print = tf.cond(tf.math.is_nan(sample_loss),
+        #                 true_fn=lambda: tf.Print(sample_loss, [sample_loss], "Sample loss is null "),
+        #                 false_fn=lambda: tf.no_op())
+        # with tf.control_dependencies([print]):
+        return sample_loss
     else:
         return tf.zeros(loss.get_shape())
 
@@ -150,10 +150,10 @@ def compute_surprisal_loss(model, loss, updated_states, sample_probabilities, su
         # with tf.control_dependencies([printer_0, printer_1, printer_2]):
         average_surprisal = tf.div_no_nan(tot_surprisal, non_read_samples)
         surprisal_loss = surprisal_influence * average_surprisal
-        print = tf.cond(tf.math.is_nan(surprisal_loss),
-                        true_fn=lambda: tf.Print(surprisal_loss, [surprisal_loss], "Surprisal loss is null "),
-                        false_fn=lambda: tf.no_op())
-        with tf.control_dependencies([print]):
-            return surprisal_loss
+        # print = tf.cond(tf.math.is_nan(surprisal_loss),
+        #                 true_fn=lambda: tf.Print(surprisal_loss, [surprisal_loss], "Surprisal loss is null "),
+        #                 false_fn=lambda: tf.no_op())
+        # with tf.control_dependencies([print]):
+        return surprisal_loss
     else:
         return tf.zeros(loss.get_shape())
