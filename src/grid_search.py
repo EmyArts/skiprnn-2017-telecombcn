08fed0182fa8eefb4d4e3f72be6482033a20bb3b
EmyArts <emy.msn.arts@hotmail.com>
2020-07-31 13:41:42 +0200

    Trying out gridsearch changes before actual run

--- src/grid_search.py
+++ src/grid_search.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
 
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--exp_id", type=int, help="id of the specific run")
-	parser.add_argument("--tot_exps", type=int, default=48, help="The total amount of parallel experiments")
+	parser.add_argument("--tot_exps", type=int, default=27, help="The total amount of parallel experiments")
 
 	args = parser.parse_args()
 	exp_id = args.exp_id
@@ -45,16 +45,10 @@ if __name__ == '__main__':
 	embedding_dict, probs_dict = get_embedding_dicts(50)
 	for idx, params in enumerate(ParameterGrid(command_configs)):
 		if idx % tot_exps == exp_id:
-			csv_name = 'hu' + str(params['hidden_units']) + '_bs' + str(params['batch_size']) + '_lr' + str(
-				params['learning_rate']) + '_b' + str(params['cost_per_sample']) + '_s' + str(
-				params['surprisal_cost']) + '.csv'
-			if not os.path.exists('../completed_csv' + csv_name):
-				params['epochs'] = 12
-				params['early_stopping'] = 'yes'
-				params['folder'] = '../EXP' + exp_id + '_LR' + str(params['learning_rate']) + '_BS' + str(
-					params['batch_size']) + '_HU' + str(params['hidden_units']) + '_CPS' + str(
-					params['cost_per_sample']) + '_SC' + str(params['surprisal_cost'])
-				model = SkipRNN(config_dict=params, emb_dict=embedding_dict, probs_dict=probs_dict)
-				model.train()
-			else:
-				print(f"\n\nNetwork {csv_name} already exists\n\n")
+			params['epochs'] = 12
+			params['early_stopping'] = 'yes'
+			params['folder'] = '../EXP' + str(exp_id) + '_LR' + str(params['learning_rate']) + '_BS' + str(
+				params['batch_size']) + '_HU' + str(params['hidden_units']) + '_CPS' + str(
+				params['cost_per_sample']) + '_SC' + str(params['surprisal_cost'])
+			model = SkipRNN(config_dict=params, emb_dict=embedding_dict, probs_dict=probs_dict)
+			model.train()
