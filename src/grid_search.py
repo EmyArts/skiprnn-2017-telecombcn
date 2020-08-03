e44ca870d0d8ca411951faede0207544466234d2
EmyArts <emy.msn.arts@hotmail.com>
2020-08-03 13:06:40 +0200

    Ready for new gridsearch

--- src/grid_search.py
+++ src/grid_search.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
 
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--exp_id", type=int, help="id of the specific run")
-	parser.add_argument("--tot_exps", type=int, default=27, help="The total amount of parallel experiments")
+	parser.add_argument("--tot_exps", type=int, default=12, help="The total amount of parallel experiments")
 
 	args = parser.parse_args()
 	exp_id = args.exp_id
@@ -45,10 +45,14 @@ if __name__ == '__main__':
 	embedding_dict, probs_dict = get_embedding_dicts(50)
 	for idx, params in enumerate(ParameterGrid(command_configs)):
 		if idx % tot_exps == exp_id:
-			params['epochs'] = 40
-			params['early_stopping'] = 'yes'
-			params['folder'] = '../EXP' + str(exp_id) + '_LR' + str(params['learning_rate']) + '_BS' + str(
-				params['batch_size']) + '_HU' + str(params['hidden_units']) + '_CPS' + str(
-				params['cost_per_sample']) + '_SC' + str(params['surprisal_cost'])
-			model = SkipRNN(config_dict=params, emb_dict=embedding_dict, probs_dict=probs_dict)
-			model.train()
+			csv_file = 'hu' + str(params['hidden_units']) + '_bs' + str(
+				params['batch_size']) + '_lr' + str(params['learning_rate']) + '_b' + str(
+				params['cost_per_sample']) + '_s' + str(params['surprisal_cost']) + '.csv'
+			if not os.path.exists('../csvs/' + csv_file):
+				params['epochs'] = 40
+				params['early_stopping'] = 'yes'
+				params['folder'] = '../EXP' + str(exp_id) + '_LR' + str(params['learning_rate']) + '_BS' + str(
+					params['batch_size']) + '_HU' + str(params['hidden_units']) + '_CPS' + str(
+					params['cost_per_sample']) + '_SC' + str(params['surprisal_cost'])
+				model = SkipRNN(config_dict=params, emb_dict=embedding_dict, probs_dict=probs_dict)
+				model.train()
