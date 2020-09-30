505faa1f4ce617fa62dec8d5523232d53a74aece
EmyArts <emy.msn.arts@hotmail.com>
2020-09-30 10:57:29 +0200

    Comparing best results

--- src/gridsearch.py
+++ src/gridsearch.py
@@ -41,14 +41,14 @@ command_configs = {
 	'batch_size': [64],
 	'hidden_units': [32],
 	'cost_per_sample': [1e-3],
-	'surprisal_cost': [0, 0.01]
+	'surprisal_cost': [0.001]
 }
 
 if __name__ == '__main__':
 
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--id", type=int, help="id of the specific run")
-	parser.add_argument("--tot_exps", type=int, default=20, help="The total amount of parallel experiments")
+	parser.add_argument("--tot_exps", type=int, default=10, help="The total amount of parallel experiments")
 	parser.add_argument("--trials", type=int, default=10, help="The amount of times the same network is trained.")
 	parser.add_argument("--print_gputil", type=bool, default=False,
 						help="Whether to show the GPU utilization on terminal")
